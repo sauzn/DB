94c8dcbbdb93ddd5f2115f2fd699d3042a476b3a
sauzn <603377527@qq.com>
2020-10-30 19:00:22 +0800

    Create user.sql

---
+++ script/user.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `cb-user` (
+	`id` INT(10,0) NOT NULL AUTO_INCREMENT COMMENT 'id',
+	`login_account` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '登录账号' COLLATE 'utf8_general_ci',
+	`pass_word` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '登录密码' COLLATE 'utf8_general_ci',
+	`user_name` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '用户名称' COLLATE 'utf8_general_ci',
+	`phone_number` VARCHAR(11) NOT NULL DEFAULT '' COMMENT '手机号' COLLATE 'utf8_general_ci',
+	`avatar` VARCHAR(50) NOT NULL COMMENT '头像' COLLATE 'utf8_general_ci',
+	`gender` INT(10,0) NOT NULL DEFAULT '0' COMMENT '性别',
+	`birthday` DATETIME NOT NULL COMMENT '生日',
+	`area` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '地区' COLLATE 'utf8_general_ci',
+	`create_time` DATETIME NOT NULL COMMENT '创建时间',
+	`update_time` DATETIME NOT NULL COMMENT '更新时间',
+	`create_by` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '创建人' COLLATE 'utf8_general_ci',
+	`update_by` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '更新人' COLLATE 'utf8_general_ci',
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
